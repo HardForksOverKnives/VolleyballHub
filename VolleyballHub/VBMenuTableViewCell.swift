bee24130d909a87241d86a35c9675e86ae9ded14
Connor Dunham <cjdunham2112@gmail.com>
2019-10-09 11:45:07 -0700

    Gives sliding menu background color

--- VolleyballHub/VBMenuTableViewCell.swift
+++ VolleyballHub/VBMenuTableViewCell.swift
@@ -16,6 +16,8 @@ class VBMenuTableViewCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        
+        self.backgroundColor = UIColor.lightGray
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
